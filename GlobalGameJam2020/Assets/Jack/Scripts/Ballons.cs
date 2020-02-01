e07a93efeca1171a509022f6e8513eb03eedfc8d
Jack Chateau-Loney <1701009@uad.ac.uk>
2020-02-01 20:51:38 +0000

    balloon and audio

---
+++ GlobalGameJam2020/Assets/Jack/Scripts/Ballons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballons : MonoBehaviour
+{
+
+    public GameObject balloonPoint = null;
+    GameObject go;
+    LineRenderer lr;
+    GameObject tetherPoint;
+    GameObject balloon;
+
+    public Material ropeMat;
+
+    private void Start()
+    {
+        go = gameObject;
+        lr = go.AddComponent<LineRenderer>();
+
+        tetherPoint = gameObject;
+        balloon = balloonPoint;
+    }
+
+
+    void Update()
+    {
+        lr.SetPosition(0, tetherPoint.transform.position);
+        lr.SetPosition(1, balloon.transform.position);
+        lr.material = ropeMat;
+        lr.startWidth = .2f;
+        lr.endWidth = .2f;
+    }
+}
---
+++ GlobalGameJam2020/Assets/Jack/Scripts/PlayIntroAudio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayIntroAudio : MonoBehaviour
+{
+    void Start()
+    {
+        AudioManager.instance.PlaySound("MonologueIntro");
+    }
+
+}
