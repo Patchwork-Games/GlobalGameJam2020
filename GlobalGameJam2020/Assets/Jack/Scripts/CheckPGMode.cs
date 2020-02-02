3c8f052dd471dc7232985285308c1a351b4a4c74
Jack Chateau-Loney <1701009@uad.ac.uk>
2020-02-02 14:21:20 +0000

    PGMODE

---
+++ GlobalGameJam2020/Assets/Jack/Scripts/CheckPGMode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPGMode : MonoBehaviour
+{
+
+    private void Start()
+    {
+        GetComponent<Renderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PauseMenu.GameIsPaused)
+        {
+            if (PGModeButton.PGModeOn)
+            {
+                GetComponent<Renderer>().enabled = true;
+            }
+            else
+            {
+                GetComponent<Renderer>().enabled = false;
+            }
+        }
+    }
+}
---
+++ GlobalGameJam2020/Assets/Jack/Scripts/PGModeButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PGModeButton : MonoBehaviour
+{
+
+    public static bool PGModeOn;
+    public GameObject CheckMark;
+
+    private void Awake()
+    {
+        CheckMark = transform.GetChild(1).gameObject;
+    }
+
+    public void PGMode()
+    {
+        if (PGModeOn)
+        {
+            CheckMark.SetActive(false);
+            PGModeOn = false;
+        }
+        else
+        {
+            CheckMark.SetActive(true);
+            PGModeOn = true;
+        }
+    }
+
+
+
+
+
+
+
+}
